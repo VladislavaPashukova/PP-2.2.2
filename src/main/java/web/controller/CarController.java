34fd685483313d10e036983634142fa743a25488
VladislavaPashukova <vlada.pashukova@gmail.com>
2022-10-29 19:17:07 +0300

    1

---
+++ src/main/java/web/controller/CarController.java
@@ -0,0 +1,25 @@
+package web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import web.service.CarServiceImpl;
+
+
+
+@Controller
+public class CarController {
+private final CarServiceImpl carService;
+
+    public CarController(CarServiceImpl carService) {
+        this.carService = carService;
+    }
+
+
+    @GetMapping("/cars")
+    public String printCars(@RequestParam(value = "count", required = false) Integer count, Model model) {
+        model.addAttribute("cars", carService.CAR_LIST(count));
+        return "cars";
+    }
+}
---
+++ src/main/java/web/service/CarServiceImpl.java
@@ -0,0 +1,29 @@
+package web.service;
+
+import web.model.Car;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class CarServiceImpl implements CarService {
+    private final List<Car> car;
+
+    {
+        car = new ArrayList<>();
+        car.add(new Car(1,"BMV", 12));
+        car.add(new Car(2,"Ford", 42));
+        car.add(new Car(3,"Mazda", 15));
+        car.add(new Car(4,"Audi", 46));
+        car.add(new Car(5,"Chevrolet", 24));
+    }
+    @Override
+    public List<Car> CAR_LIST(Integer count) {
+        if (count != null || count < 5) {
+            return car.subList(0, count);
+        } else {
+            return car;
+        }
+    }
+}
